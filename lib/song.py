bc39b04475aafa0e802e2fd82d46522cd473a1e0
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-03-05 21:10:45 +0300

    Completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,46 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count= {}
+    artist_count= {}
+
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(self.genre)
+        self.add_to_artists(self.artist)
+        self.add_to_artist_count(self.artist)
+        self.add_to_genre_count(self.genre)
+        
+
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre, increment=1):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += increment
+        else:
+            cls.genre_count[genre] = increment
+
+    @classmethod
+    def add_to_artist_count(cls, artist, increment=1):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += increment
+        else:
+            cls.artist_count[artist] = increment
